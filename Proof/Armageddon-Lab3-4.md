e34ed37c4dcb07662e708e36d976b11ecc48949d
ChuckKeyes <chuck37080@yahoo.com>
2026-04-19 21:46:00 -0400

    Add proof uploads and Jenkins config files

---
+++ Proof/Armageddon-Lab3-4.md
@@ -0,0 +1,2 @@
+https://github.com/ChuckKeyes/Obsidian/blob/main/Armageddon-Lab3%264/Read.md.md
+
--- test-bucket.tf
+++ test-bucket.tf
@@ -27,3 +27,93 @@ resource "aws_s3_bucket" "frontend" {
     Name = "Jenkins Bucket"
   }
 }
+
+resource "aws_s3_object" "proof_md" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Armageddon-Lab3-4.md"
+  source = "${path.module}/Proof/Armageddon-Lab3-4.md"
+  etag   = filemd5("${path.module}/Proof/Armageddon-Lab3-4.md")
+
+  content_type = "text/markdown"
+}
+
+resource "aws_s3_object" "console_output_1" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/console-output.png"
+  source = "${path.module}/Proof/console-output.png"
+  etag   = filemd5("${path.module}/Proof/console-output.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "console_output_2" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/console-output2.png"
+  source = "${path.module}/Proof/console-output2.png"
+  etag   = filemd5("${path.module}/Proof/console-output2.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "console_output_3" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/console-output3.png"
+  source = "${path.module}/Proof/console-output3.png"
+  etag   = filemd5("${path.module}/Proof/console-output3.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "jenkins_stages" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Jenkins-Stages.png"
+  source = "${path.module}/Proof/Jenkins-Stages.png"
+  etag   = filemd5("${path.module}/Proof/Jenkins-Stages.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "jenkins_trigger" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Jenkins-Stages-trigger.png"
+  source = "${path.module}/Proof/Jenkins-Stages-trigger.png"
+  etag   = filemd5("${path.module}/Proof/Jenkins-Stages-trigger.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "jenkins_webhook" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Jenkins-Stages-webhook.png"
+  source = "${path.module}/Proof/Jenkins-Stages-webhook.png"
+  etag   = filemd5("${path.module}/Proof/Jenkins-Stages-webhook.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "webhook1" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Webhook1.png"
+  source = "${path.module}/Proof/Webhook1.png"
+  etag   = filemd5("${path.module}/Proof/Webhook1.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "webhook2" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Webhook2.png"
+  source = "${path.module}/Proof/Webhook2.png"
+  etag   = filemd5("${path.module}/Proof/Webhook2.png")
+
+  content_type = "image/png"
+}
+
+resource "aws_s3_object" "webhook3" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "Proof/Webhook3.png"
+  source = "${path.module}/Proof/Webhook3.png"
+  etag   = filemd5("${path.module}/Proof/Webhook3.png")
+
+  content_type = "image/png"
+}
